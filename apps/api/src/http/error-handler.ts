a60f7690213a20b18391ef7da60c81abb315ca70
Vinícius Silva <vinisuxx@outlook.com>
2025-04-16 21:24:23 -0300

    feat(api): add NotFoundError

--- apps/api/src/http/error-handler.ts
+++ apps/api/src/http/error-handler.ts
@@ -1,17 +1,11 @@
 import type { FastifyError, FastifyReply, FastifyRequest } from 'fastify';
-import {
-  hasZodFastifySchemaValidationErrors,
-  isResponseSerializationError,
-} from 'fastify-type-provider-zod';
+import { hasZodFastifySchemaValidationErrors, isResponseSerializationError } from 'fastify-type-provider-zod';
 
 import { BadRequestError } from './routes/errors/bad-request-error';
+import { NotFoundError } from './routes/errors/not-found-error';
 import { UnauthorizedError } from './routes/errors/unauthorized-error';
 
-export const errorHandler = (
-  error: FastifyError,
-  req: FastifyRequest,
-  reply: FastifyReply,
-) => {
+export const errorHandler = (error: FastifyError, req: FastifyRequest, reply: FastifyReply) => {
   if (hasZodFastifySchemaValidationErrors(error)) {
     return reply.code(400).send({
       message: "Request doesn't match the schema",
@@ -34,6 +28,12 @@ export const errorHandler = (
 
   if (error instanceof UnauthorizedError) {
     return reply.status(401).send({
+      message: error.message,
+    });
+  }
+
+  if (error instanceof NotFoundError) {
+    return reply.status(404).send({
       message: error.message,
     });
   }
