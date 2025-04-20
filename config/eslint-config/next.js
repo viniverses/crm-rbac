d3089b15578cf4b448e5f27215cccc1b50f8dd37
Vinícius Silva <vinisuxx@outlook.com>
2025-04-19 21:24:52 -0300

    chore(config): modify next.js eslint config

--- config/eslint-config/next.js
+++ config/eslint-config/next.js
@@ -1,8 +1,21 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  extends: [],
-  plugins: ['simple-import-sort'],
+  extends: ['next/core-web-vitals', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+  plugins: ['@typescript-eslint', 'prettier', 'simple-import-sort'],
+  parser: '@typescript-eslint/parser',
   rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        printWidth: 120,
+        tabWidth: 2,
+        singleQuote: true,
+        trailingComma: 'all',
+        arrowParens: 'always',
+        semi: true,
+      },
+    ],
     'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
   },
 };
